53ba401eb7711db4d6bfb467c878a36835d00d28
Andrew Branch <awbranch@gmail.com>
2023-01-23 15:45:13 -0500

    Added Google Tag Manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import Page from 'components/Page';
+import TagManager from 'react-gtm-module';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-M63LRKF' });
+  });
   return (
     <>
       <Head>
